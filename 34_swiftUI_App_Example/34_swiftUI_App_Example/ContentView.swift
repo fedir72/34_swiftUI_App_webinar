0f412fc6f81339cc34be62b14c11e19e34bab5e4
fedir72 <fedir72@ua.fm>
2020-03-21 17:21:55 +0200

    add Spacer() , Divider()

--- 34_swiftUI_App_Example/34_swiftUI_App_Example/ContentView.swift
+++ 34_swiftUI_App_Example/34_swiftUI_App_Example/ContentView.swift
@@ -24,10 +24,12 @@ struct ContentView: View {
                             Text("Edit Content")
                         }).frame(width: geometry.size.width/2 - 10, height: 50).accentColor(Color.init(#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1))).background(Color.init(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1))).cornerRadius(10)                    }
                     }.padding().frame(height: 50)
+                Divider()//разделение экрана
                 Text("Hello World")
                 Text( "Banana")
-            .navigationBarTitle("SwiftBook.ru")
+                Spacer()//прижимаем все элементы вверх вью
             }
+           .navigationBarTitle("SwiftBook.ru")
         }
     }
 }
