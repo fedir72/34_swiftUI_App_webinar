d7414dd9a2456c44b1f6468ce568abaef8cba069
fedir72 <fedir72@ua.fm>
2020-03-21 17:37:29 +0200

    settings for view

--- 34_swiftUI_App_Example/34_swiftUI_App_Example/ContentView.swift
+++ 34_swiftUI_App_Example/34_swiftUI_App_Example/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            VStack{
+            VStack(alignment: .leading)  {
                 GeometryReader {geometry in
                     HStack(spacing: 20) {
                         NavigationLink(destination: ChannelView(), label: {
@@ -25,10 +25,16 @@ struct ContentView: View {
                         }).frame(width: geometry.size.width/2 - 10, height: 50).accentColor(Color.init(#colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1))).background(Color.init(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1))).cornerRadius(10)                    }
                     }.padding().frame(height: 50)
                 Divider()//разделение экрана
-                Text("Hello World")
-                Text( "Banana")
+                HStack {
+                    Image("Файл9").resizable().frame(width: 160, height: 160)//фото и настройки для него
+                    VStack(alignment: .leading ) {
+                        Text("Hello World").font(.title)//увеличиваем буквы
+                        Text( "Banana").font(.subheadline).foregroundColor(.black)
+                    }
+                }
+                
                 Spacer()//прижимаем все элементы вверх вью
-            }
+                }.padding()//отступ от краев
            .navigationBarTitle("SwiftBook.ru")
         }
     }
